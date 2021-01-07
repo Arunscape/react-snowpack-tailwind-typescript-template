16209c932f3c04852a18d3ce5bc17cb7715fbcd8
Arun Woosaree <arun@woosaree.xyz>
2021-01-07 03:53:46 +0000

    add react router to template

--- react-snowpack-tailwind-typescript/src/App.tsx
+++ react-snowpack-tailwind-typescript/src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 interface AppProps {}
 
-const App = ({}: AppProps)  => {
+const DefaultPage = () => {
   // Create the count state.
   const [count, setCount] = useState(0);
   // Create the counter (+1 every second).
@@ -15,7 +16,12 @@ const App = ({}: AppProps)  => {
   return (
     <div className="text-center">
       <header className="bg-gray-800 flex flex-col items-center justify-center text-white text-3xl min-h-screen">
-        <img src={logo} className="animate-spin-slow" alt="logo" style={{height: '40vmin', pointerEvents: 'none'}}/>
+        <img
+          src={logo}
+          className="animate-spin-slow"
+          alt="logo"
+          style={{ height: '40vmin', pointerEvents: 'none' }}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -27,9 +33,28 @@ const App = ({}: AppProps)  => {
         >
           Learn React
         </a>
+        <Link to="/otherpage">
+          <button className="text-xs">
+            React Router is included in this template
+          </button>
+        </Link>
       </header>
     </div>
   );
-}
+};
+
+const App = ({}: AppProps) => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/otherpage">
+          <p>This is another route</p>
+          <Link to="/">Go back</Link>
+        </Route>
+        <DefaultPage />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
